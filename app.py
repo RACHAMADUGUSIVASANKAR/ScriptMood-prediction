c8d72dde588e5d261aad4fd7706f1a917e9cc3b1
Rachamadugu Sivasankar <sivasankar99122@gmail.com>
2025-10-09 21:12:09 +0530

    adding bar chat

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
-# Importing essential libraries
 from flask import Flask, render_template, request
 import pickle
 
-# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
-filename = 'movie-genre-mnb-model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
+# Load model and vectorizer
+classifier = pickle.load(open('movie-genre-mnb-model.pkl','rb'))
 cv = pickle.load(open('cv-transform.pkl','rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-    	message = request.form['message']
-    	data = [message]
-    	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
-    	return render_template('result.html', prediction=my_prediction)
+    message = request.form['message']
+    data = [message]
+    vect = cv.transform(data).toarray()
+
+    # Prediction
+    my_prediction = classifier.predict(vect)
+
+    # Convert classes to list of strings
+    genres = [str(g) for g in classifier.classes_]
+    probs = [round(p * 100, 2) for p in classifier.predict_proba(vect)[0]]
+
+    # Map predicted index to genre name
+    predicted_genre = genres[int(my_prediction[0])]
+
+    return render_template(
+        'result.html',
+        prediction=my_prediction[0],        # send index
+        predicted_genre=predicted_genre,   # send actual genre name
+        genres=genres,                     # send list of genre names
+        probs=probs
+    )
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
