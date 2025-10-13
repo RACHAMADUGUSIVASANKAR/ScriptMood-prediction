59f830e84f162d99583014c5389dc31a555de848
Rachamadugu Sivasankar <sivasankar99122@gmail.com>
2025-10-13 22:58:42 +0530

    bar chat changes

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ cv = pickle.load(open('cv-transform.pkl','rb'))
 
 app = Flask(__name__)
 
+# Genre names mapped by index
+index_to_genre = [
+    "Miscellaneous", "Action", "Adventure", "Comedy", "Drama",
+    "Horror", "Romance", "Sci-Fi", "Thriller"
+]
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -20,19 +26,21 @@ def predict():
     # Prediction
     my_prediction = classifier.predict(vect)
 
-    # Convert classes to list of strings
-    genres = [str(g) for g in classifier.classes_]
+    # Probability values for each genre
     probs = [round(p * 100, 2) for p in classifier.predict_proba(vect)[0]]
 
-    # Map predicted index to genre name
+    # Get genre names for x-axis and tooltip display
+    genres = index_to_genre
+
+    # Predicted genre name
     predicted_genre = genres[int(my_prediction[0])]
 
     return render_template(
         'result.html',
-        prediction=my_prediction[0],        # send index
-        predicted_genre=predicted_genre,   # send actual genre name
-        genres=genres,                     # send list of genre names
-        probs=probs
+        prediction=my_prediction[0],        # Predicted genre index
+        predicted_genre=predicted_genre,    # Predicted genre name
+        genres=genres,                      # List of genre names
+        probs=probs                         # Probabilities
     )
 
 if __name__ == '__main__':
